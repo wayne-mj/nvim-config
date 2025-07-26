4a0b0681a48f8bb72630315ea56f586eefcd1723
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-07-27 08:27:14 +1000

    Changed the comment system

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,62 +1,15 @@
--- Comments
+-- Comments : numtostr/comment.nvim
 return {
-    "echasnovski/mini.comment",
-    version = false, -- always use latest
+    "numToStr/Comment.nvim",
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    opts = {
+
+    },
     config = function()
-        -- Set custom commentstring for TypeScript and React variants
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {
-                "typescript",
-                "typescriptreact",
-                "javascriptreact",
-            },
-            callback = function()
-                vim.bo.commentstring = "{ /*** %s ***/ }"
-            end,
-        })
-
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {
-                "fortran",
-                "f90", "f95", "f03", "f08"
-            },
-            callback = function()
-                vim.bo.commentstring = "! %s !"
-            end
-        })
-
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {
-                "cobol",
-                "cob", "cbl"
-            },
-            callback = function()
-                vim.bo.commentstring = "* > %s < "
-            end
-        })
-
-        -- Setup mini.comment with default mappings
-        require("mini.comment").setup({
-            -- Optional: you can change mappings or other options here
-            mappings = {
-                comment = "gc",
-                comment_line = "gcc",
-                textobject = "gc",
-            },
-            options = {
-                ignore_blank_line = false,
-                start_of_line = true,
-                pad_comment_parts = true,
-            },
-            hooks = {
-                pre = function()
-                    local cs = vim.bo.commentstring
-                    local prefix = cs:match("^(.-)%%s")
-                    return function(line)
-                        return not vim.startswith(vim.trim(line), vim.trim(prefix or ""))
-                    end
-                end,
-            },
-        })
+        require("Comment").setup {
+            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        }
     end,
 }
